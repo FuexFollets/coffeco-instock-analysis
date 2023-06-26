e30c89611ed83d1c1c06beeca020f5670a8641ef
FuexFollets <fuex9follets@gmail.com>
2023-06-26 19:20:09 -0400

    Removed 'amount' field from product model

--- data_management/database/models/product.py
+++ data_management/database/models/product.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Optional
 
 
 @dataclass
@@ -9,4 +8,3 @@ class Product:
     id: bytes
     name: str
     price: float
-    amount: Optional(int)
