039ed2831e8057b5efba1b01f29f7375e83adf5d
FuexFollets <fuex9follets@gmail.com>
2023-06-29 09:22:26 -0400

    Fixed database interface

--- data_management/database/interface.py
+++ data_management/database/interface.py
@@ -10,18 +10,34 @@ class Database:
         self.connection: sqlite3.Connection = sqlite3.connect(self.path)
         self.cursor: sqlite3.Cursor = self.connection.cursor()
 
-    def table_names(self) -> list[str]:
+    def query_table_names(self) -> list[str]:
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
         return [table[0] for table in self.cursor.fetchall()]
 
     def create_table(self, name: str, columns: list[str]):
-        self.cursor.execute(f"CREATE TABLE {name} ({', '.join(columns)});")
+        try:
+            self.cursor.execute(f"CREATE TABLE {name} ({', '.join(columns)});")
+            return self
 
-    def insert(self, table: str, values: list[str]):
-        self.cursor.execute(f"INSERT INTO {table} VALUES ({', '.join(values)});")
+        except sqlite3.OperationalError:
+            return self
 
-    def query_table(self, table: str) -> list[tuple]:
+    def insert_into_table(self, table: str, values: list[str]):
+        values_string: str = ', '.join(self.query_table_columns(table))
+        insert_string: str = ', '.join('?' * len(values))
+
+        self.cursor.execute(f"INSERT INTO {table}({values_string}) VALUES({insert_string});", values)
+        self.connection.commit()
+
+        return self
+
+    def query_table_columns(self, table: str) -> list[str]:
+        self.cursor.execute(f"PRAGMA table_info({table});")
+
+        return [column[1] for column in self.cursor.fetchall()]
+
+    def query_table_values(self, table: str) -> list[tuple]:
         self.cursor.execute(f"SELECT * FROM {table};")
 
         return self.cursor.fetchall()
