208285281acf3b0650ef19bff777fbb51b05a992
FuexFollets <fuex9follets@gmail.com>
2023-07-01 11:08:04 -0400

    Added product identification

--- data_management/database/models/product.py
+++ data_management/database/models/product.py
@@ -6,7 +6,12 @@ from dataclasses import dataclass
 @dataclass
 class Product:
     id: bytes
+
+    sku: str
+    fn_sku: str
+    asin: str
     name: str
+
     price: float
 
     # Add other fields as needed
