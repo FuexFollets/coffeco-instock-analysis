d8d0910b2db69b475e1e296264bd0a31986955cb
FuexFollets <fuex9follets@gmail.com>
2023-07-03 13:59:12 -0400

    Implemented new model for users

--- data_management/database/models/user.py
+++ data_management/database/models/user.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from data_management.database.models.model import Model
+from data_management.database.wrapper import SQLDataType
+
 
 @dataclass
-class User:
-    id: bytes
+class User(Model, sql_table_name="users"):
+    id: SQLDataType.TEXT.value.py_type
     email: str
     token: bytes
