2ba08cc458dd62d840cc9401a3f8111233a0e2d0
FuexFollets <fuex9follets@gmail.com>
2023-07-09 19:12:41 -0400

    Removed redundant field constants

--- data_management/constants.py
+++ data_management/constants.py
@@ -2,5 +2,3 @@ from __future__ import annotations
 
 USER_DB_PATH: str = "data/users.db"
 USER_DB_TABLE_NAME: str = "users"
-USER_DB_TABLE_COLUMNS: list[str] = ["id", "email", "token"]
-USER_DB_TABLE_COLUMN_TYPES: list[str] = ["INTEGER PRIMARY KEY", "TEXT", "TEXT"]
