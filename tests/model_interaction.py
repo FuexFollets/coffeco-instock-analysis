646a8b6f6e161fbdcd2342fea85a6072df8bab27
FuexFollets <fuex9follets@gmail.com>
2023-07-09 11:32:51 -0400

    Added tests for row insertion

--- tests/model_interaction.py
+++ tests/model_interaction.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from data_management.database.models.model import Model
+from data_management.database.wrapper import SQLDataType, SQLDatabase, SQLTable
 
 
 @dataclass
@@ -10,4 +11,15 @@ class MyModel(Model, sql_table_name = "testtable"):
     name: str
     f1: float
 
-instance = MyModel("test", 1.0)
+
+@dataclass
+class MatchingModel(Model, sql_table_name = "testtable"):
+    name: bytes
+    age: bytes
+    f1: float
+    f2: float
+
+my_db: SQLDatabase = SQLDatabase("./test.db")
+testtable: SQLTable = my_db.query_table("testtable")
+
+instance = MatchingModel(None, b"bobby", b"sammy", 1.0, 2.0)
