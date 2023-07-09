2aa22091fec07d6973862512543470334cce3f74
FuexFollets <fuex9follets@gmail.com>
2023-07-09 19:13:08 -0400

    Implemented UserModel class

--- data_management/database/models/user.py
+++ data_management/database/models/user.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from data_management.database.models.model import Model
-from data_management.database.wrapper import SQLDataType
 
 
 @dataclass
-class User(Model, sql_table_name="users"):
-    id: SQLDataType.TEXT.value.py_type
+class UserModel(Model, sql_table_name="users"):
+    id: bytes
     email: str
     token: bytes
+
