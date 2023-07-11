501f084f11aefd51404d7e254ff547a62d949418
FuexFollets <fuex9follets@gmail.com>
2023-07-11 11:24:27 -0400

    Added module exposing code to __init__.py

---
+++ data_management/__init__.py
@@ -0,0 +1,26 @@
+from typing import Sequence
+
+from data_management.database.wrapper import (
+    SQLDataType,
+    SQLType,
+    SQLColumn,
+    SQLTable,
+    SQLDatabase,
+)
+from data_management.database.models.model import Model
+from data_management.database.models.user import UserModel
+from data_management.constants import (
+    USER_DB_PATH,
+    USER_DB_TABLE_NAME,
+)
+
+
+__all__: Sequence[str] = (
+    "SQLDataType",
+    "SQLType",
+    "SQLColumn",
+    "SQLTable",
+    "SQLDatabase",
+    "Model",
+    "UserModel",
+)
