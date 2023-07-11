d49b3e21ebffb949749dae1f09a0ce6fffd43e48
FuexFollets <fuex9follets@gmail.com>
2023-07-11 14:01:28 -0400

    Added credential file reading

--- data_management/constants.py
+++ data_management/constants.py
@@ -1,4 +1,11 @@
 from __future__ import annotations
 
+import tomllib
+
 USER_DB_PATH: str = "data/users.db"
 USER_DB_TABLE_NAME: str = "users"
+
+CREDENTIALS: dict[str, str]
+
+with open("credentials.toml", "r") as file:
+    CREDENTIALS = tomllib.loads(file.read())
