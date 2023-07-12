ecf4e1f38d4891859daa7062a1dfbcb6bf34afd1
FuexFollets <fuex9follets@gmail.com>
2023-07-12 19:53:52 -0400

    Made function call for inventory summary

--- data_management/collection.py
+++ data_management/collection.py
@@ -1,3 +1,8 @@
 from __future__ import annotations
 
 from sp_api.api import Inventories
+
+from data_management.constants import CREDENTIALS
+
+
+res = Inventories(credentials=CREDENTIALS).get_inventory_summary_marketplace()
